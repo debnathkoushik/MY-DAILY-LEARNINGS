07afa831d2d0ac9c2aa5a467cde41333d40f26b3
Koushik Debnath <debnathkoushik605@gmail.com>
2024-02-28 20:35:14 +0530

    personal data updated

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,8 +1,8 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
+  title: 'MY-DAILY-LEARNINGS',
+  author: 'Koushik Debnath',
+  headerTitle: 'MY-DAILY-LEARNINGS',
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
   theme: 'system', // system, dark or light
